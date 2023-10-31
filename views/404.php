247f9e75a2a6184c6bdad0094f040afde5255e90
srdjanculibrk987 <srdjanculibrk@yahoo.com>
2023-10-31 22:09:44 +0100

    404.php fajl ispravka

--- views/404.php
+++ views/404.php
@@ -1,14 +1,15 @@
 <?php require('partials/head.php') ?>
 <?php require('partials/nav.php') ?>
 
-<main>
-    <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-        <h1 class="text-2xl font-bold">Sorry. Page Not Found.</h1>
+<main role="main" class="container">
+    <div class="my-5">
+        <h1 class="display-4">Sorry. Page Not Found.</h1>
 
-        <p class="mt-4">
-            <a href="/" class="text-blue-500 underline">Go back home.</a>
+        <p class="lead">
+            <a href="/" class="btn btn-primary">Go back home</a>
         </p>
     </div>
 </main>
+
 
 <?php require('partials/footer.php') ?>
